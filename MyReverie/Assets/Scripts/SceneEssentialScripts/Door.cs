4faa0b014a2b700b3dfc786ea38f0b5b85edd2d1
Malte Hølledig <malte@holledig.dk>
2017-12-18 16:22:29 +0100

    portal and spiral door

--- MyReverie/Assets/Scripts/SceneEssentialScripts/Door.cs
+++ MyReverie/Assets/Scripts/SceneEssentialScripts/Door.cs
@@ -8,7 +8,7 @@ public class Door : MonoBehaviour {
 	public bool midPoint;
 	public bool endPoint;
 	public string nextLevelName;
-	public Color openColor, closedColor;
+	private SpriteRenderer openSprite, lockedSprite;
 
 	//Privates
 	private GameObject isKey;
@@ -23,12 +23,8 @@ public class Door : MonoBehaviour {
 
 	void Start () 
 	{
-		sRendererEnd = transform.GetChild(0).GetComponent<SpriteRenderer>();
-		//sRendererMid = transform.GetChild(1).GetComponent<SpriteRenderer>();
-		mainSystem = transform.GetChild(3).GetComponent<ParticleSystem>().main;
-		myTextMesh = transform.GetComponentInChildren<TextMesh>();
-		pSystem = transform.GetChild(4).GetComponent<ParticleSystem>();
-
+		openSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+		lockedSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
 		myCollider = GetComponent<BoxCollider2D>();
 		isKey = (GameObject.FindWithTag("key") == null)?null:GameObject.FindWithTag("key").gameObject;
 
@@ -60,17 +56,17 @@ public class Door : MonoBehaviour {
 	{
 		if (!needKey) //open
 		{
-			//myTextMesh.color = openColor;
-			mainSystem.startColor = openColor;
-			sRendererEnd.color = openColor;
-			//sRendererMid.color = openColor;
+			openSprite.enabled = true;
+			lockedSprite.enabled = false;
 		}
 		else if (needKey) //closed
 		{
-			//myTextMesh.color = closedColor;
-			mainSystem.startColor = closedColor;
-			sRendererEnd.color = closedColor;
-			//sRendererMid.color = closedColor;
+			lockedSprite.enabled = true;
+			openSprite.enabled = false;
+		}
+		if (needKey && Key.hasKey) {
+			openSprite.enabled = true;
+			lockedSprite.enabled = false;
 		}
 	}
 	void OnTriggerEnter2D(Collider2D other)
